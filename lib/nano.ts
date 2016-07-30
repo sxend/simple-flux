6766d2e6b617aecbd7564557320405ef43cce5d3
sxend <arimitsu.k+github@gmail.com>
2016-07-30 21:17:24 +0900

    add nano

---
+++ lib/nano.ts
@@ -0,0 +1,8 @@
+/* Nano Templates - https://github.com/trix/nano */
+export default function nano(template: string, data: any) {
+  return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+    var keys = key.split("."), v = data[keys.shift()];
+    for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+    return (typeof v !== "undefined" && v !== null) ? v : "";
+  });
+}
